adf852138158a7041e7a696912626dbfbe94784d
Jon Wedaman <jwedaman@bullhorn.com>
2014-02-20 17:52:38 -0500

    whitespace

--- resume/generate_resume.py
+++ resume/generate_resume.py
@@ -8,7 +8,7 @@ context = yaml.load(open(YAML_FILE))
 
 skills = context["resume"]["Skills"]
 for skillObj in skills:
-	skillObj["list"] = ', '.join( skillObj["list"] )
+    skillObj["list"] = ', '.join( skillObj["list"] )
 
 with open(OUTPUT_FILE,'w') as ff:
     ff.write( pystache.render(open(TEMPLATE_FILE).read(), context) )
